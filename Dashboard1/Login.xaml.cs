c58d162fd11871cb7364fe913c36d868cc80037c
Achmad Furqon <achmdfurqon@gmail.com>
2020-01-24 08:08:47 +0700

    first commit

---
+++ Dashboard1/Login.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Configuration;
+using Dapper;
+
+namespace Dashboard1
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+
+    public class UserLogin
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
+    }
+
+    public partial class Login : Window
+    {
+        
+        public Login()
+        {
+            InitializeComponent();
+
+        }
+
+        private void ButtonLogin_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["connectDB"].ConnectionString);
+
+            var check = sqlConnection.QueryAsync<UserLogin>("exec SP_Login @Username, @Password",
+                new { Username = userLogin.Text, Password = passwordLogin.Password }).Result.SingleOrDefault();
+
+            if (check != null)
+            {
+                //if (check.role == "admin")
+                //{
+                    MessageBox.Show("Login as Admin");
+                //Show();
+                //}
+                //else
+                //{
+                //    MessageBox.Show("Login as User");
+                //}
+
+            }
+            else
+            {
+                MessageBox.Show("Login is Failed");
+            }
+        }
+    }
+}
---
+++ Dashboard1/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using Dapper;
+
+namespace Dashboard1
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string birthPlace { get; set; }
+        public string birthDate { get; set; }
+        public string nik { get; set; }
+        public string religion { get; set; }
+        public string npwp { get; set; }
+        public string graduate { get; set; }
+        public string university { get; set; }
+        public string email { get; set; }
+        public string joinDate { get; set; }
+        public string status { get; set; }
+        public string departmentid { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["connectDB"].ConnectionString);
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Insert(object sender, RoutedEventArgs e)
+        {
+
+            var check = sqlConnection.QueryAsync<UserLogin>("exec SP_Insert_Employee @id, @name, @phone, @address, @birthPlace," +
+                "@birthDate, @nik, @religion, @npwp, @graduate, @university, @email, @joinDate, @status, @departmentId",
+                new
+                {
+                    id = idEmployee.Text,
+                    name = nameEmployee.Text,
+                    phone = phone.Text,
+                    address = address.Text,
+                    birthPlace = bPlace.Text,
+                    birthDate = bDate,
+                    nik = nik.Text,
+                    religion = religion.Text,
+                    npwp = npwp.Text,
+                    graduate = graduate.Text,
+                    university = university.Text,
+                    email = email.Text,
+                    joinDate = joinDate.Text,
+                    status = status.Text,
+                    departmentId = departmentId.Text
+                });
+        }
+
+        private void Button_Update(object sender, RoutedEventArgs e)
+        {
+            var check = sqlConnection.QueryAsync<UserLogin>("exec SP_Update_Employee @id, @name, @phone, @address, @birthPlace," +
+                "@birthDate, @nik, @religion, @npwp, @graduate, @university, @email, @joinDate, @status, @departmentId",
+                new
+                {
+                    id = idEmployee.Text,
+                    name = nameEmployee.Text,
+                    phone = phone.Text,
+                    address = address.Text,
+                    birthPlace = bPlace.Text,
+                    birthDate = bDate,
+                    nik = nik.Text,
+                    religion = religion.Text,
+                    npwp = npwp.Text,
+                    graduate = graduate.Text,
+                    university = university.Text,
+                    email = email.Text,
+                    joinDate = joinDate.Text,
+                    status = status.Text,
+                    departmentId = departmentId.Text
+                });
+        }
+
+        private void Button_Delete(object sender, RoutedEventArgs e)
+        {
+            var check = sqlConnection.QueryAsync<UserLogin>("exec SP_Delete_Employee @id",
+                new { id = idEmployee.Text });
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GridTitleBar_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
